ff55eb56cf1dc21bc4a2b6db914b63b99bc3f33c
Phil <phil@secdev.org>
2014-01-15 17:31:21 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'rebus',
+    version = '0.1',
+    packages=[ 'rebus' ],
+
+    # Metadata
+    author = 'Philippe Biondi',
+    author_email = 'phil@secdev.org',
+    description = 'REbus: Reverse Engineering Bus',
+)
