5153b1a8e82843db899f01d854a8e86047e5467d
Phil <phil@secdev.org>
2014-02-21 19:00:21 +0100

    Added wait agent

---
+++ rebus/agents/wait.py
@@ -0,0 +1,24 @@
+import sys
+from rebus.agent import Agent
+
+@Agent.register
+class Wait(Agent):
+    _name_ = "wait"
+    _desc_ = "Wait for some selectors and ouput their value to stdout"
+    @classmethod
+    def add_arguments(cls, subparser):
+        subparser.add_argument("selectors", nargs="+",
+                               help="Dump selector values on stdout")
+    def init_agent(self):
+        self.wait_for = []
+    def selector_filter(self, selector):
+        if self.wait_for:
+            for s in self.wait_for:
+                if selector.startswith(s):
+                    return True
+        return False
+    def process(self, desc, sender_id):
+        print repr(desc.value[:500])
+
+    def run(self, options):
+        self.wait_for += options.selectors
