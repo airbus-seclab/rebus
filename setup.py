10eeca72a9c8d22a59cf963699b7cb9a5f5a312e
Xavier Mehrenberger <xavier.mehrenberger@eads.net>
2014-04-25 16:29:51 +0200

    Add web_interface templates & static files to setup.py.

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ setup(
     name = 'rebus',
     version = '0.1',
     packages=[ 'rebus', 'rebus/buses', 'rebus/buses/dbusbus', 'rebus/agents', 'rebus/tools'],
+    package_data={'rebus/agents': ['static/*.js',
+        'static/*.css',
+        'static/bootstrap-3.1.1-dist/css/*',
+        'static/bootstrap-3.1.1-dist/fonts/*',
+        'static/bootstrap-3.1.1-dist/js/*',
+        'static/jquery-file-upload/*',
+        'templates/*']},
     scripts = [ 'bin/rebus_master_dbus', 'bin/rebus_agent' ],
     data_files = [
         ('etc/rebus', ['conf/dbus_session.conf']),
