0b48d8c98eae4c3ffe3ac1f4ec2252eca8ce6272
Phil <phil@secdev.org>
2014-09-18 10:53:23 +0200

    Force absolute import names in importer

--- rebus/agents/__init__.py
+++ rebus/agents/__init__.py
@@ -1,3 +1,3 @@
 from rebus.tools.importer import importer_for
 
-import_all = importer_for(__file__)
+import_all = importer_for(__file__, __name__)
--- rebus/tools/importer.py
+++ rebus/tools/importer.py
@@ -2,11 +2,12 @@ import logging
 
 log = logging.getLogger("rebus.importer")
 
-def importer_for(path):
+def importer_for(path, prefix):
     def import_all(path=path, stop_on_error=False):
         import os,pkgutil
         folder = os.path.dirname(path)
         for importer,name,_ in pkgutil.iter_modules([folder]):
+            name = prefix+"."+name
             loader = importer.find_module(name)
             try:
                 loader.load_module(name)
