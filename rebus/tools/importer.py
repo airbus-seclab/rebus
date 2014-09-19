babbedbbe3f0162d407c3cca73241d40b7c239d3
Phil <phil@secdev.org>
2014-09-19 12:58:59 +0200

    Fix importer to set references to imported modules into parent module

--- rebus/tools/importer.py
+++ rebus/tools/importer.py
@@ -7,15 +7,18 @@ def importer_for(path, prefix):
     def import_all(path=path, stop_on_error=False):
         import os,pkgutil
         folder = os.path.dirname(path)
+        module = sys.modules[prefix]
         for importer,name,_ in pkgutil.iter_modules([folder]):
-            name = prefix+"."+name
-            if name in sys.modules:
+            absname = prefix+"."+name
+            if absname in sys.modules:
                 continue
-            loader = importer.find_module(name)
+            loader = importer.find_module(absname)
             try:
-                loader.load_module(name)
+                submod = loader.load_module(absname)
             except ImportError,e:
                 if stop_on_error:
                     raise
                 log.warning("Cannot load REbus plugin [%s]. Root cause: %s" % (name,e))
+            else:
+                setattr(module, name, submod)
     return import_all
