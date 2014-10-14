4da17fd1adcf0efb30fce33ae1c30cfc8d0543a2
Phil <phil@secdev.org>
2014-10-14 14:35:53 +0200

    Added link_finder agent

---
+++ rebus/agents/link_finder.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import os
+import hashlib
+from collections import defaultdict
+from rebus.agent import Agent
+from rebus.descriptor import Descriptor
+
+@Agent.register
+class LinkFinder(Agent):
+    _name_ = "link_finder"
+    _desc_ = "Find messages that are related and notify about it"
+
+    def __init__(self, bus, name=None, domain='default'):
+        Agent.__init__(self, bus, name=name, domain=domain)
+        self.memories = defaultdict(list)
+        
+    def process(self, descriptor, sender_id):
+        sel = descriptor.selector
+        p = sel.find("%")
+        pth,hsh = sel[:p],sel[p:]
+        try:
+            val = hashlib.md5(descriptor.value).digest()
+        except TypeError:
+            val = hashlib.md5(str(descriptor.value)).digest()
+        key = (pth,val)
+        if key in self.memories:
+            related = self.memories[key]
+            self.log.debug("%r related to %r" % (sel, related))
+            for r in related:
+                rel_sel = pth+r
+                rel_desc = self.get(self.domain, rel_sel)
+                self.declare_link(descriptor, rel_desc, pth, "Same value for %s" % pth)
+        self.memories[key].append(hsh)
+
