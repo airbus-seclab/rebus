19261187654b46fccad30491e4a21f4ae9830ce7
Phil <phil@secdev.org>
2014-10-28 18:29:59 +0100

    Fixed tar archive parsing (extract only files) and factorized untarring

--- rebus/agents/unarchive.py
+++ rebus/agents/unarchive.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
 
+import os
+import time
 from rebus.agent import Agent
 from rebus.descriptor import Descriptor
 from rebus.agents.inject import guess_selector
-import time
 from StringIO import StringIO
 
 
@@ -18,22 +19,28 @@ class Unarchive(Agent):
             selector.startswith("/compressed/")
 
     def process(self, descriptor, sender_id):
+        import tarfile
         start = time.time()
         data = descriptor.value
         selector = descriptor.selector
 
         #: List of (unarchived file name, unarchived file contents)
         unarchived = []
+
+        def do_untar(archive, mode, archive_label=descriptor.label, unarchived=unarchived):
+            tar = tarfile.open(fileobj=StringIO(archive), mode=mode)
+            for finfo in tar.getmembers():
+                if finfo.isfile() and finfo.size > 0:
+                    fname = os.path.basename(finfo.name)
+                    unarchived.append((archive_label + ":" + fname,
+                                       tar.extractfile(finfo).read()))
+
+
         # Compressed files
         if "/compressed/bzip2" in selector:
             # Try and extract - might be a .tar.bz2
-            import tarfile
             try:
-                tar = tarfile.open(fileobj=StringIO(descriptor.value),
-                                   mode="r:bz2")
-                for fname in tar.getnames():
-                    unarchived.append((descriptor.label + ":" + fname,
-                                       tar.extractfile(fname).read()))
+                do_untar(descriptor.value, "r:bz2")
             except tarfile.TarError:
                 # Probably not a compressed tar file
                 import bz2
@@ -41,13 +48,8 @@ class Unarchive(Agent):
                 unarchived.append(("bunzipped %s" % descriptor.label, data))
         if "/compressed/gzip" in selector:
             # Try and extract - might be a .tar.gz
-            import tarfile
             try:
-                tar = tarfile.open(fileobj=StringIO(descriptor.value),
-                                   mode="r:gz")
-                for fname in tar.getnames():
-                    unarchived.append((descriptor.label + ":" + fname,
-                                       tar.extractfile(fname).read()))
+                do_untar(descriptor.value, "r:gz")
             except tarfile.TarError:
                 # Probably not a compressed tar file
                 from gzip import GzipFile
@@ -57,11 +59,7 @@ class Unarchive(Agent):
 
         # Archive files
         if "/archive/tar" in selector:
-            import tarfile
-            tar = tarfile.open(fileobj=StringIO(descriptor.value))
-            for fname in tar.getnames():
-                unarchived.append((descriptor.label + ":" + fname,
-                                   tar.extractfile(fname).read()))
+            do_untar(descriptor.value, mode=None)
         if "/archive/zip" in selector:
             from zipfile import ZipFile
             fzip = ZipFile(file=StringIO(descriptor.value))
