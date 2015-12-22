4e79e37c47b0ffa9ed7b95de69536318c3c5fbdc
Raphaël Rigo <raphael.rigo@airbus.com>
2015-12-22 17:44:59 +0100

    Add secure_subprocess module for firejail

---
+++ rebus/tools/secure_subprocess.py
@@ -0,0 +1,58 @@
+import subprocess
+import os
+import distutils.spawn
+import logging
+
+TMP = 1 # Writable access to system wide /tmp
+UNIX = 1 << 1 # Unix sockets
+FS = 1 << 2 # full filesystem
+
+log = logging.getLogger("rebus.secure_subprocess")
+
+firejail = distutils.spawn.find_executable("firejail")
+secure = []
+if firejail is None:
+    ch = logging.StreamHandler()
+    log = logging.getLogger("rebus.secure_subprocess.initialization")
+    log.addHandler(ch)
+    log.warning("firejail binary not found, using INSECURE non sandboxed mode!")
+else:
+    secure.append(firejail)
+    # TODO add private-etc
+    secure.extend(["--noprofile", "--quiet", "--nosound", "--private-dev", "--env=TMPDIR="+os.environ['HOME'],
+                  "--private", "--seccomp", ])
+
+def make_firejail_cmdline(flags, cmd):
+    if secure == []:
+        return cmd
+
+    if flags is None:
+        flags = 0
+
+    secres = list(secure)
+
+    if flags&FS == FS:
+        # TODO: find a way to specify specific files
+        secres.remove("--private")
+    elif flags&TMP == TMP:
+        # TODO be more restrictive
+        pass
+    else:
+        secres.append("--read-only=/*")
+
+    if flags&UNIX == UNIX:
+        secres.append("--net=unix")
+    else:
+        secres.append("--net=none")
+
+    full_cmd = secres + ["--"] + cmd
+
+    return full_cmd
+
+def check_output(flags, cmd, *args, **kwargs):
+    full_cmd = make_firejail_cmdline(flags, cmd)
+    return subprocess.check_output(full_cmd, *args, **kwargs)
+
+def Popen(flags, cmd, *args, **kwargs):
+    full_cmd = make_firejail_cmdline(flags, cmd)
+    return subprocess.Popen(full_cmd, *args, **kwargs)
