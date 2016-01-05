b335b56ed2e39687b508bd52e041a5f478556d33
Xavier Mehrenberger <xavier.mehrenberger@airbus.com>
2016-01-05 15:10:19 +0100

    dotrenderer: use secure_subprocess.

--- rebus/agents/dotrenderer.py
+++ rebus/agents/dotrenderer.py
@@ -1,4 +1,5 @@
 import subprocess
+from rebus.tools import secure_subprocess
 from rebus.agent import Agent
 
 
@@ -19,8 +20,10 @@ class DotRenderer(Agent):
         # ex. /graph/dot/qum/minhash%1234 is a qumgraph based on minhash
         # distances
         graphsrc, datatype = descriptor.selector.split('%')[0].split('/')[3:5]
-        p = subprocess.Popen(["neato", "-Tsvg"],
-                             stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        p = secure_subprocess.Popen(
+            flags=0, cmd=["neato", "-Tsvg"], stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE)
+
         out, err = p.communicate(input=dot)
 
         d2 = descriptor.spawn_descriptor(
